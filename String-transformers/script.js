91052145f8c8f79891dec49681efaf3f471b97f8
Aditya Ojha <thisisadityaojha@gmail.com>
2024-12-03 05:51:54 +0530

    Update script.js

--- String-transformers/script.js
+++ String-transformers/script.js
@@ -1,1 +1,21 @@
+ lowercaseop.innerText = input.value.toLowerCase()
+ uppercaseop.innerText = input.value.toUpperCase()
 
+ //adding logic so that typing happens with every word typed as input
+
+ input.addEventListener('input',(e) => {
+    lowercaseop.innerText = input.value.toLowerCase()
+    uppercaseop.innerText = input.value.toUpperCase()
+    camelcaseop.innerText = input.value.
+    pascalcaseop.innerText = input.value.
+    snakecaseop.innerText = input.value.
+    kebabcaseop.innerText = input.value.
+    trimop.innerText = input.value.
+
+ })
+
+ //creatting a function for camel case typing 
+
+ function toCamelCase(){
+      
+ }
